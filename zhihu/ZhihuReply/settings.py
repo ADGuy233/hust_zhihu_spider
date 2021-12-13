d5daf6df943bab5090e4eaa75c97434eaf11f1b7
ADGuy233 <76994592+ADGuy233@users.noreply.github.com>
2021-12-13 12:41:16 +0800

    update

--- zhihu/ZhihuReply/settings.py
+++ zhihu/ZhihuReply/settings.py
@@ -7,10 +7,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'zhihu'
+BOT_NAME = 'ZhihuReply'
 
-SPIDER_MODULES = ['zhihu.spiders']
-NEWSPIDER_MODULE = 'zhihu.spiders'
+SPIDER_MODULES = ['ZhihuReply.spiders']
+NEWSPIDER_MODULE = 'ZhihuReply.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -41,9 +41,6 @@ DEFAULT_REQUEST_HEADERS = {
    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
    'Accept-Language': 'en',
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-    'cookie':'_zap=b8487359-95b8-471c-8f0d-15a769d1032e;'
-             ' d_c0="ANBbFp3l5xGPTmp0unQOjQTYLzMMOM8yxts=|1600400115"; '
-             '_ga=GA1.2.809078608.1600400120; _xsrf=TWyhmiyMmvpC4G1iya2UEdD3dI8mWdsn; _9755xjdesxxd_=32; YD00517437729195:WM_TID=ipzKuKwpK+BFFEAFBAJ+07kzCgHvBevm; __snaker__id=TCvgXmAmnGFpMNxw; captcha_session_v2="2|1:0|10:1637481450|18:captcha_session_v2|88:eFNxOU5tZHN6a0RIM1pSeVM1aTlRM2lhVjlIN3RpZjJOVkNTTTVvTGw1a3h4VmRGcU1hNzBKaHdVYyt5RmtRNg==|5d50d212eddd8ed992af37a6eade88cf8956ff14dd248a096abc029c273aeeaa"; gdxidpyhxdE=WXS/mqX2dZp4urzya4jKnljDun30Ha0\QEMEIjCN1T71EGRNuQCQG+TQu4Dz9cdL/qHuC3ysU018z+w5HK2iuC9rdqsrEDzLBTRHQjSeRKD+61OoycyO+Y7nGlKa+wX1IIgU7EXGpCzq9wnCvUjqMvmw+rY4NHfrgJKP/qcdo2D+EkiX:1637482351714; YD00517437729195:WM_NI=0oKvnSeXO9MUR80BaFvck+/PFSqFvnuIMD7fgmrsWlaBAw4C0IxzSv/V5ZyGfaoCQxMqKm87199P8Z7cWSkCHJfAkGL+Smvz+37wVioyud2lg+9XunjpvTyVitqtHud+QlE=; YD00517437729195:WM_NIKE=9ca17ae2e6ffcda170e2e6ee8dc77f8397ff96cb47f48a8fa3d15e838b9b85b633f19b8d88d84a958887b2d42af0fea7c3b92af4b18597ed59a78881dad05f92e800a3e44eb6bda5ccae5c868dbfa7f566b79783a5b66ea5b3be94d668a39e8f98b749f497b89bd27eed8afbbbfc4da6bfa1a4d4748faf858ebb3df3e7f7b1bb52f5bd99b4ea498c96aaa2ea4d90f59d8ef572e9edf998d563ada7bb85b168a5f5f7b9f866aab0bab5fc5eabacbf95cb7fa8f59f8dc837e2a3; captcha_ticket_v2="2|1:0|10:1637481459|17:captcha_ticket_v2|704:eyJ2YWxpZGF0ZSI6IkNOMzFfNzlrcUVydEZQMTZYaXU0d05kLnJSdklYaFE0OVA5NXA2VjRVODR1MU1rR0xZb2hUVHVhNF9tTklCVGVTbmxjVjdaRXNEazlvTUpUdGo5QVZFY3NvRUlwQ2U0Vkh2cTVLeHZHdElMWnk5OWRDVzg4T2lIMVV2MEl2S1Z5dmRHMHlJU3VxbmFLcVFXaW1EdjRpMUNNYkp2MHpiUnhlbzdLeDlNanJZX2VSV1A0SHlUelBENjRfWmIwLTkuLUptLkU2VG44LUM1WHZCNDY5dWFGRnhGYWR3b3p5dVQ4UDhpcXd5NlJsS1hPeF9FR09xLU9pVlY5Sm1feXYwcGFKX1VrTlprdmM2d2JPcy4xVkJqdjlEWlFjb29MQzRkV1NDd2NXQ01FaXEtZmNZNXNuamJBTS5ZLmtXTUF3NllVbFlFaURDN2t3Q00tLVlMbzZXRUg4UnlyLWdFdTZTQy5oTTZiUWdEcjlDS0d2THZZT081Y3Nac0F1QmpzV3JudWNycVJjdWg5dzFMYWhqZzZHMU5SYTJhNEpnUVh5TDRyem43ODVDMHNkWEE5eHpEdXRIb3NZTHJ6QlVjNjZudHZMa1JYRXUtaVN3OEM2bnNpTUlpVy56czcucjlOdWMxS3A1bVNCTnFGX0U1RG02LmR4MDFXZU5vRmQwekFYcEFaMyJ9|59d15d3e12b1e38b09c7f27c48bf6aa88cdcded5e269e1887ab1fbcdd5d9ab39"; z_c0="2|1:0|10:1637481475|4:z_c0|92:Mi4xY0lHZEh3QUFBQUFBMEZzV25lWG5FU1lBQUFCZ0FsVk5BMHFIWWdDV3REUUNJblRTcHUzM3FQU1dRcVVNVTgzLUxB|289cc68a94ba58f00f9dccd849a1e23e2ba43e2e1b8acf1577e3364de374bff1"; q_c1=38db7461569348bfb52b8415d3b4ffc1|1637641249000|1601711881000; __utmv=51854390.100--|2=registration_date=20201003=1^3=entry_date=20201003=1; tst=r; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1637902299,1638108251,1638160132,1638361206; NOT_UNREGISTER_WAITING=1; SESSIONID=9mm3NnCfybrqDtJFf9zutrHGCBDI3oWBlEB993wG6u0; JOID=Vl0UBkh557_zmlc9XnhhYKLpJsBKQY7WxtM4ejwHhMmG2CR2HASPGpOcUj1crDUnY0DLWcbJtVFmazUuNQTJPzE=; osd=U1gQBkl84rvzm1I4WnhgZaftJsFPRIrWx9Y9fjwGgcyC2CVzGQCPG5aZVj1dqTAjY0HOXMLJtFRjbzUvMAHNPzA=; __utma=51854390.809078608.1600400120.1638160144.1638361211.6; __utmb=51854390.0.10.1638361211; __utmc=51854390; __utmz=51854390.1638361211.6.2.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/topic/19566933/hot; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1638361243; KLBRSID=4efa8d1879cb42f8c5b48fe9f8d37c16|1638361396|1638361206',
 }
 
 # Enable or disable spider middlewares
--- zhihu/ZhihuReply/spiders/ZhihuReply.py
+++ zhihu/ZhihuReply/spiders/ZhihuReply.py
@@ -5,12 +5,12 @@ class ZhihureplySpider(scrapy.Spider):
     name = 'ZhihuReply'
 
     def start_requests(self):
-        question_list = []
+        question_list = [496963656]
         yield scrapy.Request(
                 url='https://www.zhihu.com/api/v4/questions/{}/answers?'
                     'include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cvip_info%2Cbadge%5B*%5D.topics%3Bdata%5B*%5D.settings.table_of_content.enabled'
                     '&offset=0&limit=20&sort_by=updated'.format(question_list[0]),
-                callback=self.parse)
+                callback=self.answer_parse)
 
     def question_parse(self,response):
         is_end = json.loads(response.body.decode("utf-8"))["paging"]['is_end']
@@ -36,9 +36,9 @@ class ZhihureplySpider(scrapy.Spider):
             updated_time = answer['updated_time']
             comment_count = answer['comment_count']
             voteup_count = answer['voteup_count']
-            content = answer['content'].get('message')
-            yield items.Reply(aid=aid, qid=qid, updated_time=updated_time, author=author,
-                              comment_count=comment_count, voteup_count=voteup_count, content=content)
+            content = answer['content']
+            '''yield items.Reply(aid=aid, qid=qid, updated_time=updated_time, author=author,
+                              comment_count=comment_count, voteup_count=voteup_count, content=content)'''
             if comment_count != 0:
                 yield scrapy.Request(
                     url='https://www.zhihu.com/api/v4/answers/{}/root_comments?order=normal&limit=20&offset=0&status=open'.format(aid),
@@ -55,15 +55,17 @@ class ZhihureplySpider(scrapy.Spider):
         aid = response.url.split('/')[6]
         for comment in comments:
             cid = comment['id']
+            print('cid:', cid)
             author = comment['author']['member']['name']
             updated_time = comment['created_time']
             child_comment_count = comment['child_comment_count']
             vote_count = comment['vote_count']
             content = comment['content'].get('message')
-            yield items.Reply(aid=aid, cid=cid, updated_time=updated_time, author=author,
-                              comment_count=child_comment_count, voteup_count=vote_count, content=content)
+            '''yield items.Reply(aid=aid, cid=cid, updated_time=updated_time, author=author,
+                              comment_count=child_comment_count, voteup_count=vote_count, content=content)'''
             if child_comment_count <= 2:
-                yield items.Reply()
+                print(comment['child_comments']['id'])
+                #yield items.Reply()
             else:
                 yield scrapy.Request(url='https://www.zhihu.com/api/v4/comments/{}/child_comments'.format(cid),
                                      callback=self.child_comment_parse)
@@ -79,19 +81,20 @@ class ZhihureplySpider(scrapy.Spider):
         cid = response.url.split('/')[6]
         for comment in comments:
             ccid = comment['id']
+            print('ccid',ccid)
             author = comment['author']['member']['name']
             reply_to_author = comment['reply_to_author']['member']['name']
             updated_time = comment['created_time']
             child_comment_count = comment['child_comment_count']
             vote_count = comment['vote_count']
             content = comment['content'].get('message')
-            yield items.Reply(cid=cid, ccid=ccid, reply_to_author=reply_to_author, updated_time=updated_time, author=author,
-                              comment_count=child_comment_count, voteup_count=vote_count, content=content)
+            '''yield items.Reply(cid=cid, ccid=ccid, reply_to_author=reply_to_author, updated_time=updated_time, author=author,
+                              comment_count=child_comment_count, voteup_count=vote_count, content=content)'''
         if is_end:
             pass
         else:
             yield scrapy.Request(url=json.loads(response.body.decode("utf-8"))["paging"]['next'],
-                                 callback=self.comment_parse)
+                                 callback=self.child_comment_parse)
 
 
 
